ad2abbf8b7224e865d2ec4a52021951db0dc9a0f
mayurmarvel <45968855+mayurmarvel@users.noreply.github.com>
2022-03-21 20:47:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let count = 0;
+let currentCount = document.getElementById('currentCount');
+
+currentCount.innerText = count;
+
+let increment= function(){
+count += 1
+currentCount.innerText = count;
+
+}
+
+let decrement= function(){
+if(!count <=0)
+count -= 1
+currentCount.innerText = count;
+
+}
